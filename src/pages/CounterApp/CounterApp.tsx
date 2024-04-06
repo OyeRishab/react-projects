ee843587678127e7b08ee437edaaafd3539a59ba
Rishab Jain <85238177+OyeRishab@users.noreply.github.com>
2024-04-06 19:00:33 +0530

    Counter App

---
+++ src/pages/CounterApp/CounterApp.tsx
@@ -0,0 +1,5 @@
+function CounterApp() {
+  return <div>CounterApp</div>;
+}
+
+export default CounterApp;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home/Home";
+import CounterApp from "@/pages/CounterApp/CounterApp";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/counter" element={<CounterApp />} />
     </Routes>
   );
 }
